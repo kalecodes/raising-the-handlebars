bdaa1a4d446644a8d3106a0f94e06f7acadc3e17
kalen510 <kalen.wiley@gmail.com>
2022-03-13 21:21:28 -0400

    undo changes

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_OLLIVE_URL
-    ? new Sequelize(process.env.JAWSDB_OLLIVE_URL)
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
