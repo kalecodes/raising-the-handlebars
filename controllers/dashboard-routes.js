5a6ddfc4ebb5e4048b1a41bc3e848b62b165eecf
kalen510 <kalen.wiley@gmail.com>
2022-03-14 21:19:40 -0400

    remove excess thens in dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -57,48 +57,11 @@ router.get('/', withAuth, (req, res) => {
         })
 
     })
-    // .then(dbEventData => {
-
-    //     res.render('dashboard', {
-    //         events,
-    //         loggedIn: req.session.loggedIn
-    //     });
-    // })
-    // .then(() => {
-        
-    // })
     .catch(err => {
         console.log(err);
         res.status(500).json(err)
     });
 });
-
-// NEED TO FIND A WAY TO GET EVENTS AND TAGS FOR DASHBAORD
-// get all tags
-// router.get('/', withAuth, (req, res) => {
-//     Tag.findAll({
-//         attributes: [
-//             'id',
-//             'tag_title'
-//         ]
-//     })
-//     .then(dbTagData => {
-//         console.log('hey')
-//         if (!dbTagData) {
-//             res.status(404).json({ message: 'No tags found'})
-//         }
-//         const tags = dbTagData.map(tag => tag.get({ plain: true }));
-//         console.log(tags);
-//         res.render('dashboard', {
-//             tags,
-//             loggedIn: req.session.loggedIn
-//         })
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err)
-//     });
-// });
 
 
 module.exports = router;
